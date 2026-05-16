630afdf8dd24b458245cf0ef5ad525e6fe427342
Frazer Herron <frazerherron66@gmail.com>
2026-05-17 00:22:39 +0100

    fix: set nodejs20.x runtime for vercel

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,7 +2,9 @@ import { defineNitroConfig } from "nitro/config"
 
 export default defineNitroConfig({
   preset: "vercel",
-  experimental: {
-    wasm: false,
-  },
+  vercel: {
+    config: {
+      runtime: "nodejs20.x"
+    }
+  }
 })
